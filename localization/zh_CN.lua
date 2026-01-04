114144b718846e8bf5e384f3e99672c5795b05d1
normalcat <tntgav@gmail.com>
2026-01-04 14:22:37 -0800

    fix locs

--- localization/zh_CN.lua
+++ localization/zh_CN.lua
@@ -1374,7 +1374,7 @@ return {
 			},
 			j_cry_arsonist = {
 				name = "纵火犯",
-				text = { "打出{C:attention}葫芦{}牌型时，摧毁所有计分牌" },
+				text = "打出{C:attention}葫芦{}牌型时，摧毁所有计分牌",
 			},
 			j_cry_astral_bottle = {
 				name = "瓶中星球",
@@ -5161,11 +5161,11 @@ return {
 				"52张牌的牌组中的每一张牌",
 				"你疯了?",
 			},
-			["cry_None"] = { "出牌为 0 张牌" },
+			["cry_None"] = "出牌为 0 张牌",
 
-			["cry_Declare0"] = { "始终算作顺子" },
-			["cry_Declare1"] = { "始终算作同花" },
-			["cry_Declare2"] = { "始终算作葫芦" },
+			["cry_Declare0"] = "始终算作顺子",
+			["cry_Declare1"] = "始终算作同花",
+			["cry_Declare2"] = "始终算作葫芦",
 			["cry_Declare0_suitless"] = { "始终算作顺子", "手牌不需要特定花色" },
 			["cry_Declare1_suitless"] = { "始终算作同花", "手牌不需要特定花色" },
 			["cry_Declare2_suitless"] = { "始终算作葫芦", "手牌不需要特定花色" },
@@ -5421,8 +5421,8 @@ return {
 			a_round_minus = {
 				"-#1# 回合",
 			},
-			a_tag_minus = { "-#1# 标签" },
-			a_tags_minus = { "-#1# 标签" },
+			a_tag_minus = "-#1# 标签",
+			a_tags_minus = "-#1# 标签",
 			a_tag = {
 				"#1# 标签",
 			},
